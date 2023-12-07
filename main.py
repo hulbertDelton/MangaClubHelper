52c33ec134972ba2f6cb041261cd917d2c2142d4
delton <hulbert.delton@gmail.com>
2023-12-07 12:54:40 -0700

    switching to use decouple instead of os.getenv

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
 from discord.ext.commands.help import HelpCommand
+from decouple import config
 import manga_utils, manga_picking
-    
-#TOKEN = os.getenv('DISCORD_TOKEN')
-TOKEN = os.getenv('DISCORD_TOKEN')
+
+TOKEN = config('TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
